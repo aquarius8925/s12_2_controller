24145fb7b30ebfc73c63886859de1f90f4f536ca
user <aquarius8925@gmail.com>
2026-04-10 11:40:02 +0900

    back

--- s12_2_controller/src/main/java/com/lgy/s12_2_controller/Java.java
+++ s12_2_controller/src/main/java/com/lgy/s12_2_controller/Java.java
@@ -2,6 +2,6 @@ package com.lgy.s12_2_controller;
 
 public class Java {
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!!");
+		System.out.println("Hello World");
 	}
 }
